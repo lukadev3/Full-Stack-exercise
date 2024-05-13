a920af19ffed8dbb627245736224b0fb6c52e6c1
Luka Ivanovic <93-2021@pmf.kg.ac.rs>
2024-05-13 19:35:43 +0200

    created components and service

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailsFormComponent } from './payment-details/payment-details-form/payment-details-form.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentDetailsComponent
+    PaymentDetailsComponent,
+    PaymentDetailsFormComponent,
+    PaymentDetailFormComponent
   ],
   imports: [
     BrowserModule,
