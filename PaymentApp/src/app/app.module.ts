62793345ba15328d8ef4ff41d322633373ab75f6
Luka Ivanovic <93-2021@pmf.kg.ac.rs>
2024-05-13 19:21:04 +0200

    payment-details component

--- PaymentApp/src/app/app.module.ts
+++ PaymentApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent
   ],
   imports: [
     BrowserModule,
